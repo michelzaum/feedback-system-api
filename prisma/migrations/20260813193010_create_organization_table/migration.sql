ae70c531440ca2cc1560516b446022c655c93463
michelzaum <michel.oliveira999@hotmail.com>
2026-08-13 16:31:20 -0300

    chore: project setup and Organization model creation

---
+++ prisma/migrations/20260813193010_create_organization_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "organization" (
+    "id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "organization_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "organization_slug_key" ON "organization"("slug");
