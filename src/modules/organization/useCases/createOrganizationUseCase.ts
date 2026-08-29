86ce07f5c68d83ab97ce9f752af569fb79ab8879
michelzaum <michel.oliveira999@hotmail.com>
2026-08-29 17:23:18 -0300

    feat: implement CreateOrganizationUseCase class

---
+++ src/modules/organization/useCases/createOrganizationUseCase.ts
@@ -0,0 +1,12 @@
+import type { IOrganizationRepository } from "../repositories/IOrganizationRepository";
+import type { IUseCase } from "../../../shared/interfaces/IUseCase";
+import type { ICreateOrganization } from "../interfaces/ICreateOrganization";
+import type { IOrganization } from "../interfaces/IOrganization";
+
+export class CreateOrganizationUseCase implements IUseCase<ICreateOrganization, IOrganization> {
+  constructor(private readonly organizationRepository: IOrganizationRepository) { }
+
+  async execute(data: ICreateOrganization): Promise<IOrganization> {
+    return await this.organizationRepository.create(data);
+  }
+}
