fd246f6f95a0c67fd5f2354d5501d1aff5439b05
michelzaum <michel.oliveira999@hotmail.com>
2026-09-07 16:51:20 -0300

    feat: create InMemoryMembershipRepository class

---
+++ src/modules/membership/repositories/InMemoryMembershipRepository.ts
@@ -0,0 +1,57 @@
+import type { IMembership } from "../interfaces/IMembership";
+import type { ICreateMembershipRepositoryInput } from "./interfaces/ICreateMembershipRepository";
+import type { IMembershipRepository } from "./interfaces/IMembershipRepository";
+
+export class InMemoryMembershipRepository implements IMembershipRepository {
+  private memberships: IMembership[] = [];
+
+  create(data: ICreateMembershipRepositoryInput): Promise<IMembership> {
+    const membership: IMembership = {
+      organizationId: data.organizationId,
+      userId: data.userId,
+      role: data.role,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    this.memberships.push(membership);
+    return Promise.resolve(membership);
+  }
+
+  update(
+    organizationId: string,
+    userId: string,
+    data: Partial<Pick<ICreateMembershipRepositoryInput, "role">>,
+  ): Promise<IMembership | undefined> {
+    const membership = this.memberships.find(
+      (item) => item.organizationId === organizationId && item.userId === userId,
+    );
+
+    if (!membership) {
+      return Promise.resolve(undefined);
+    }
+
+    membership.role = data.role ?? membership.role;
+    membership.updatedAt = new Date();
+
+    return Promise.resolve(membership);
+  }
+
+  delete(organizationId: string, userId: string): Promise<void> {
+    this.memberships = this.memberships.filter(
+      (item) => !(item.organizationId === organizationId && item.userId === userId),
+    );
+    return Promise.resolve();
+  }
+
+  async findMembership(
+    organizationId: string,
+    userId: string,
+  ): Promise<IMembership | null> {
+    const membership =
+      this.memberships.find(
+        (item) => item.organizationId === organizationId && item.userId === userId,
+      ) || null;
+    return Promise.resolve(membership);
+  }
+}
