258d344ba1abc0abe7975e06d56d60c522874321
michelzaum <michel.oliveira999@hotmail.com>
2026-09-07 16:44:52 -0300

    feat: create IMembershipRepository interface

---
+++ src/modules/membership/repositories/interfaces/IMembershipRepository.ts
@@ -0,0 +1,16 @@
+import type { IMembership } from "../../interfaces/IMembership";
+import type { ICreateMembershipRepositoryInput } from "./ICreateMembershipRepository";
+
+export interface IMembershipRepository {
+  create(data: ICreateMembershipRepositoryInput): Promise<IMembership>;
+  update(
+    organizationId: string,
+    userId: string,
+    data: Partial<Pick<ICreateMembershipRepositoryInput, "role">>,
+  ): Promise<IMembership | undefined>;
+  delete(organizationId: string, userId: string): Promise<void>;
+  findByOrganizationIdAndUserId(
+    organizationId: string,
+    userId: string,
+  ): Promise<IMembership | null>;
+}
