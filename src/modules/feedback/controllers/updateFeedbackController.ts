e92157e0c158c5230b8e18343c01f16b9df3bcc5
michelzaum <michel.oliveira999@hotmail.com>
2026-09-08 18:03:40 -0300

    feat: create UpdateFeedbackController class and request interfaces

---
+++ src/modules/feedback/controllers/updateFeedbackController.ts
@@ -0,0 +1,25 @@
+import type { Request, Response } from "express";
+import type { UpdateFeedbackUseCase } from "../useCases/updateFeedbackUseCase";
+import type { IUpdateFeedbackRequestParams } from "./interfaces/IUpdateFeedbackRequestParams";
+import type { IUpdateFeedbackRequestBody } from "./interfaces/IUpdateFeedbackRequestBody";
+
+export class UpdateFeedbackController {
+  constructor(private readonly updateFeedbackUseCase: UpdateFeedbackUseCase) { }
+
+  async handle(request: Request<IUpdateFeedbackRequestParams, any, IUpdateFeedbackRequestBody>, response: Response) {
+    const { id } = request.params;
+    const { statusId } = request.body;
+
+    try {
+      const feedback = await this.updateFeedbackUseCase.execute({ id, statusId });
+
+      if (!feedback) {
+        return response.status(404).json({ error: "Feedback not found" });
+      }
+
+      return response.json(feedback);
+    } catch (error: any) {
+      return response.status(400).json({ error: error.message });
+    }
+  }
+}
