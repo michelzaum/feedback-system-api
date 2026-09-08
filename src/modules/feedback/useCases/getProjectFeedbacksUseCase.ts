4c6c3361ab3238c0cdde0092563aaf4d54c63f3b
michelzaum <michel.oliveira999@hotmail.com>
2026-09-08 17:58:25 -0300

    feat: create GetProjectFeedbacksUseCase class

---
+++ src/modules/feedback/useCases/getProjectFeedbacksUseCase.ts
@@ -0,0 +1,12 @@
+import type { IFeedbackRepository } from "../repositories/interfaces/IFeedbackRepository";
+import type { IUseCase } from "../../../shared/interfaces/IUseCase";
+import type { IGetFeedbacksByProject } from "../interfaces/IGetFeedbacksByProject";
+import type { IFeedback } from "../interfaces/IFeedback";
+
+export class GetProjectFeedbacksUseCase implements IUseCase<IGetFeedbacksByProject, IFeedback[]> {
+  constructor(private readonly feedbackRepository: IFeedbackRepository) { }
+
+  async execute(data: IGetFeedbacksByProject): Promise<IFeedback[]> {
+    return await this.feedbackRepository.findFeedbacksByProjectId(data.projectId);
+  }
+}
