cbb19f4a9c0068f6bd648c070ef7c4b2faf44f18
michelzaum <michel.oliveira999@hotmail.com>
2026-09-16 18:15:01 -0300

    feat: fix CORS and cookie config for cross-origin sign-in

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -34,7 +34,7 @@ import { makeSignInController } from '../modules/sign-in/factories/makeSignInCon
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 
 app.post('/sign-in', async (req, res) => makeSignInController().handle(req, res));
